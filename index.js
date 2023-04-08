cd58695ecdf898dd08140d3a58449008bf2cd1c7
Dalfour <yjulius.mail@gmail.com>
2023-04-08 17:43:59 +1000

    finalising sort function

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ function setupSearchListener(searchBarId, callback) { // Callback function from 
 
 setupSearchListener('search__bar', handleSearchTerm);
 
-// Use the search term to fetch the data
+let result // make result global so we can use it in other functions
 
+// Use the search term to fetch the data
 async function handleSearchTerm(searchTerm) { 
   // Fetch the data from the API
   const url = await fetch(`https://www.omdbapi.com/?apikey=1fd6092d&s=${searchTerm}`);
-  const result = await url.json();
+  result = await url.json();
   const movieElement = document.querySelector(".movies");
   movieElement.innerHTML = result.Search.slice(0, 6).map((result) => movieHTML(result)).join("");
 }
@@ -54,3 +55,14 @@ function movieHTML(result) {  // Create the HTML for the movie
             </div>
             `;
 }
+
+// Sort movies by year either low to high or high to low
+function sortMovies(event) {
+    if (event.target.value === 'LOW_TO_HIGH') {
+      result.Search.sort((a, b) => a.Year - b.Year);
+    } else if (event.target.value === 'HIGH_TO_LOW') {
+      result.Search.sort((a, b) => b.Year - a.Year);
+    }
+    const movieElement = document.querySelector(".movies");
+    movieElement.innerHTML = result.Search.slice(0, 6).map((result) => movieHTML(result)).join("");
+  }
